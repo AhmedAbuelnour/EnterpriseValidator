f0d61375ecc84d91138575ea5b779d31bec71064
Ahmed Abuelnour <AhmedAbuelnoor@outlook.com>
2021-12-18 16:26:10 +0200

    IsValidJsonRule

---
+++ EnterpriseValidator/ValidatorRules/Strings/IsValidJsonRule.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+namespace EnterpriseValidator.ValidatorRules.Strings;
+public record IsValidJsonRule<T>(string ValidationMessage) : IValidationRule<T>
+{
+    public ValueTask<bool> Check(T value)
+    {
+        if (value == null)
+            return new ValueTask<bool>(false);
+
+        if (value is not string input)
+            return new ValueTask<bool>(false);
+
+        try
+        {
+            JsonDocument.Parse(input);
+            return new ValueTask<bool>(true);
+        }
+        catch(JsonException)
+        {
+            return new ValueTask<bool>(false);
+        }
+    }
+}
+
