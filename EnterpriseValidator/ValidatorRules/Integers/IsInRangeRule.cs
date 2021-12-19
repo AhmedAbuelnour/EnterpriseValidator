e37a41145ee6ec820d414ed7f49d9bb8e4b86acb
Ahmed Abuelnour <AhmedAbuelnoor@outlook.com>
2021-12-19 19:30:31 +0200

    .

--- EnterpriseValidator/ValidatorRules/Integers/IsInRangeRule.cs
+++ EnterpriseValidator/ValidatorRules/Integers/IsInRangeRule.cs
@@ -1,18 +1,16 @@
 ﻿using System.Threading.Tasks;
-
-namespace EnterpriseValidator.ValidatorRules.Integers
+namespace EnterpriseValidator.ValidatorRules.Integers;
+public record IsInRangeRule<T>(int Start, int End, string ValidationMessage) : IValidationRule<T>
 {
-    public record IsInRangeRule<T>(int Start, int End, string ValidationMessage) : IValidationRule<T>
+    public ValueTask<bool> Check(T value)
     {
-        public ValueTask<bool> Check(T value)
-        {
-            if (value == null)
-                return new ValueTask<bool>(false);
+        if (value == null)
+            return new ValueTask<bool>(false);
 
-            if (value is not int input)
-                return new ValueTask<bool>(false);
+        if (value is not int input)
+            return new ValueTask<bool>(false);
 
-            return new ValueTask<bool>(input >= Start && input <= End);
-        }
+        return new ValueTask<bool>(input >= Start && input <= End);
     }
 }
+
